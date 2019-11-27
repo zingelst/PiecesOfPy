94386ad017d6ccd2837e51249478d59b8d637b5a
Steve Z <zingelst@gmail.com>
2019-11-26 23:45:48 -0500

    Working on an argparse with subparsers example

---
+++ cmd/argparse_subparse.py
@@ -0,0 +1,10 @@
+# Example of using argparse with sub parsers, useful for
+# building interfaces like that of the docker cli
+
+import argparse
+
+def run():
+    print("IOU on argparse example")
+
+if __name__ == "__main__":
+    run()
